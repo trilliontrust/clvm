6348fe2519f927304fbac27b9cae5360c6cd31bb
Richard Kiss <him@richardkiss.com>
2020-02-27 16:05:18 -0800

    Use an EvalContext.

--- src/api.rs
+++ src/api.rs
@@ -1,5 +1,5 @@
 use super::eval::Reduction;
-use super::eval::{eval, EvalErr, FApply2};
+use super::eval::{run_program, EvalErr, FApply3};
 use super::f_table::make_f_lookup;
 use super::serialize::{node_from_stream, node_to_stream};
 use super::sexp::Node;
@@ -36,29 +36,24 @@ fn do_eval(
     py: Python<'static>,
     form_u8: &PyBytes,
     env_u8: &PyBytes,
-    apply2: PyObject,
+    apply3: PyObject,
     op_quote: u8,
     op_args: u8,
 ) -> PyResult<(String, Vec<u8>, u32)> {
     let sexp = node_from_bytes(form_u8.as_bytes())?;
     let env = node_from_bytes(env_u8.as_bytes())?;
     let f_table = make_f_lookup();
-    let mut py_apply2: FApply2 = Box::new(move |sexp, args| -> Result<Reduction, EvalErr> {
-        let byte_vec: Vec<u8> = apply2
-            .call1(py, (node_to_bytes(&sexp)?, node_to_bytes(&args)?))?
-            .extract(py)?;
-        let bytes: &[u8] = &byte_vec;
-        Ok(Reduction(node_from_bytes(bytes)?, 1000))
-    });
-    let r = eval(
-        &sexp,
-        &env,
-        0,
-        100_000,
-        &f_table,
-        &mut py_apply2,
-        op_quote,
-        op_args,
+    let py_apply3: FApply3 = Box::new(
+        move |_eval_context, sexp, args| -> Result<Reduction, EvalErr> {
+            let byte_vec: Vec<u8> = apply3
+                .call1(py, (node_to_bytes(&sexp)?, node_to_bytes(&args)?))?
+                .extract(py)?;
+            let bytes: &[u8] = &byte_vec;
+            Ok(Reduction(node_from_bytes(bytes)?, 1000))
+        },
+    );
+    let r = run_program(
+        &sexp, &env, 0, 100_000, &f_table, py_apply3, op_quote, op_args,
     );
     match r {
         Ok(Reduction(node, cycles)) => Ok(("".into(), node_to_bytes(&node)?, cycles)),
--- src/eval.rs
+++ src/eval.rs
@@ -7,8 +7,6 @@ pub struct Reduction(pub Node, pub u32);
 
 pub type FApply = fn(&Node) -> Result<Reduction, EvalErr>;
 
-pub type FApply2 = Box<dyn FnMut(&Node, &Node) -> Result<Reduction, EvalErr>>;
-
 pub type FLookup = [Option<FApply>; 256];
 
 impl From<std::io::Error> for EvalErr {
@@ -33,125 +31,7 @@ impl Node {
     }
 }
 
-fn eval_params(
-    params: &Node,
-    env: &Node,
-    current_cost: u32,
-    max_cost: u32,
-    f_table: &FLookup,
-    mut apply2: &mut FApply2,
-    op_quote: u8,
-    op_args: u8,
-) -> Result<Reduction, EvalErr> {
-    let iter = params.clone();
-    let mut new_cost = current_cost;
-    let mut vec: Vec<Node> = Vec::new();
-    for item in iter {
-        let r = eval(
-            &item,
-            &env,
-            new_cost,
-            max_cost,
-            f_table,
-            &mut apply2,
-            op_quote,
-            op_args,
-        )?;
-        vec.push(r.0);
-        new_cost += r.1;
-        if new_cost >= max_cost {
-            return item.err("exceed max cost");
-        }
-    }
-    Ok(Reduction(Node::from_list(vec), new_cost))
-}
-
-fn apply(
-    operator: &Node,
-    params: &Node,
-    f_table: &FLookup,
-    apply2: &mut FApply2,
-) -> Result<Reduction, EvalErr> {
-    let op_8: Option<u8> = operator.clone().into();
-    if let Some(op_8) = op_8 {
-        if let Some(f) = f_table[op_8 as usize] {
-            return f(&params);
-        }
-    };
-    apply2(operator, params)
-}
-
-pub fn eval(
-    form: &Node,
-    env: &Node,
-    current_cost: u32,
-    max_cost: u32,
-    f_table: &FLookup,
-    apply2: &mut FApply2,
-    op_quote: u8,
-    op_args: u8,
-) -> Result<Reduction, EvalErr> {
-    match form.as_pair() {
-        None => form.err("not a list"),
-        Some((left, right)) => {
-            if left.is_pair() {
-                let r = eval(
-                    &left,
-                    &env,
-                    current_cost,
-                    max_cost,
-                    f_table,
-                    apply2,
-                    op_quote,
-                    op_args,
-                )?;
-                match r {
-                    Reduction(result, new_cost) => eval(
-                        &result.first()?,
-                        &result.rest()?,
-                        new_cost,
-                        max_cost,
-                        f_table,
-                        apply2,
-                        op_quote,
-                        op_args,
-                    ),
-                }
-            } else {
-                let as_operator: Option<u8> = left.clone().into();
-                if let Some(opcode) = as_operator {
-                    if opcode == op_quote {
-                        return {
-                            let rest = form.rest()?;
-                            if rest.nullp() || !rest.rest()?.nullp() {
-                                form.err("quote requires exactly 1 parameter")
-                            } else {
-                                Ok(Reduction(right.first()?, current_cost + 1))
-                            }
-                        };
-                    } else if opcode == op_args {
-                        return { Ok(Reduction(env.clone(), current_cost + 1)) };
-                    }
-                }
-                let Reduction(params, new_cost) = eval_params(
-                    &right,
-                    env,
-                    current_cost,
-                    max_cost,
-                    f_table,
-                    apply2,
-                    op_quote,
-                    op_args,
-                )?;
-
-                let Reduction(r, apply_cost) = apply(&left, &params, f_table, apply2)?;
-                Ok(Reduction(r, apply_cost + new_cost))
-            }
-        }
-    }
-}
-
-pub type FEval = fn(&Eval, &Node, u32, u32, u8, u8) -> Result<Reduction, EvalErr>;
+pub type FEval = fn(&Eval, &Node, &Node, u32, u32, u8, u8) -> Result<Reduction, EvalErr>;
 
 pub struct Eval {
     pub eval_f: FEval,
@@ -180,6 +60,7 @@ pub fn default_apply0(
 pub fn default_eval(
     eval_context: &Eval,
     form: &Node,
+    env: &Node,
     current_cost: u32,
     max_cost: u32,
     op_quote: u8,
@@ -192,6 +73,7 @@ pub fn default_eval(
                 let r = (eval_context.eval_f)(
                     &eval_context,
                     &left,
+                    &env,
                     current_cost,
                     max_cost,
                     op_quote,
@@ -200,7 +82,8 @@ pub fn default_eval(
                 match r {
                     Reduction(result, new_cost) => (eval_context.eval_f)(
                         eval_context,
-                        &result,
+                        &result.first()?,
+                        &result.rest()?,
                         new_cost,
                         max_cost,
                         op_quote,
@@ -220,18 +103,18 @@ pub fn default_eval(
                             }
                         };
                     } else if opcode == op_args {
-                        return { Ok(Reduction(left.clone(), current_cost + 1)) };
+                        return { Ok(Reduction(env.clone(), current_cost + 1)) };
                     }
                 }
                 let Reduction(params, new_cost) = eval_params2(
                     &eval_context,
                     &form,
+                    &env,
                     current_cost,
                     max_cost,
                     op_quote,
                     op_args,
                 )?;
-
                 let Reduction(r, apply_cost) =
                     (eval_context.apply_f)(&eval_context, &left, &params)?;
                 Ok(Reduction(r, apply_cost + new_cost))
@@ -243,20 +126,20 @@ pub fn default_eval(
 fn eval_params2(
     eval_context: &Eval,
     params: &Node,
+    env: &Node,
     current_cost: u32,
     max_cost: u32,
     op_quote: u8,
     op_args: u8,
 ) -> Result<Reduction, EvalErr> {
-    let iter = params.first()?.clone();
-    let env = params.rest()?.clone();
+    let iter = params.rest()?;
     let mut new_cost = current_cost;
     let mut vec: Vec<Node> = Vec::new();
     for item in iter {
-        let new_node = Node::pair(&item, &env);
         let r = (eval_context.eval_f)(
             &eval_context,
-            &new_node,
+            &item,
+            &env,
             new_cost,
             max_cost,
             op_quote,
@@ -271,21 +154,41 @@ fn eval_params2(
     Ok(Reduction(Node::from_list(vec), new_cost))
 }
 
-use crate::f_table::make_f_lookup;
-
 pub fn default_apply3(
-    eval_context: &Eval,
+    _eval_context: &Eval,
     operator: &Node,
-    args: &Node,
+    _args: &Node,
 ) -> Result<Reduction, EvalErr> {
     operator.err("unknown operator")
 }
 
-pub fn make_default_eval_context() -> Eval {
+pub fn make_default_eval_context(f_lookup: FLookup, apply3: FApply3) -> Eval {
     Eval {
         eval_f: default_eval,
         apply_f: default_apply0,
-        f_table: make_f_lookup(),
-        apply3: Box::new(default_apply3),
+        f_table: f_lookup,
+        apply3: Box::new(apply3),
     }
+}
+
+pub fn run_program(
+    form: &Node,
+    env: &Node,
+    current_cost: u32,
+    max_cost: u32,
+    f_table: &FLookup,
+    apply3: FApply3,
+    op_quote: u8,
+    op_args: u8,
+) -> Result<Reduction, EvalErr> {
+    let eval_context: Eval = make_default_eval_context(*f_table, apply3);
+    (eval_context.eval_f)(
+        &eval_context,
+        &form,
+        &env,
+        current_cost,
+        max_cost,
+        op_quote,
+        op_args,
+    )
 }
